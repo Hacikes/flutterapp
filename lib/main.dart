ad4b12a88ffd90544bf6a1cb60d960dfcfb9857a
Hacikes <volna4753@gmail.com>
2022-11-09 11:18:04 +0300

    Вынес виджет в отдельный класс + теперь автоматически работает Hot Reload

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,41 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: const Text('Alpha'),
-      backgroundColor: Colors.black87,
-    ),
-    body: const Center(
-      child: Text(
-        'hello',
-        style: TextStyle(
-          fontSize: 20.0,
-          fontWeight: FontWeight.bold,
-          letterSpacing: 2.0,
-          color: Colors.grey,
-          fontFamily: 'PoppinsExtraBold',
-          ),
-      ),
-    ),
-    floatingActionButton: FloatingActionButton(
-      onPressed: () {  },
-      backgroundColor: Colors.black87,
-      child: const Text('click'),
-    ),
-  ),
+void main() => runApp(const MaterialApp(
+  // Используем экземпляр виджета Home
+  home: Home(),
 ));
+
+
+// Благодаря такой компановке можно переиспользовать этот виджет где угодно
+// и + к этому не надо каждый раз тыкать Hot Reload
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Alpha'),
+        backgroundColor: Colors.black87,
+      ),
+      body: const Center(
+        child: Text(
+          'hello',
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            color: Colors.grey,
+            fontFamily: 'PoppinsExtraBold',
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {  },
+        backgroundColor: Colors.black87,
+        child: const Text('click me'),
+      ),
+    );
+  }
+}
 
